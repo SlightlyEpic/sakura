dde0e20c74e8d55e9c3c5f509fa3dd0ef5de4913
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2025-07-10 00:04:38 +0530

    feat: overengineered boilerplate

--- compile-serv/cmd/main.go
+++ compile-serv/cmd/main.go
@@ -1,13 +1,73 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"os/signal"
+	"sync"
 	"time"
+
+	"github.com/danielgtaylor/huma/v2"
+	"github.com/danielgtaylor/huma/v2/adapters/humago"
 )
 
+const port = "8080"
+
 func main() {
-	fmt.Println("compile service started")
-	for {
-		time.Sleep(5 * time.Second)
+	ctx := context.Background()
+	ctx, cancel := signal.NotifyContext(ctx, os.Interrupt)
+	defer cancel()
+
+	if err := run(ctx, os.Stdout, os.Stderr); err != nil {
+		fmt.Fprintf(os.Stderr, "%s\n", err)
+		os.Exit(1)
 	}
+}
+
+func run(
+	ctx context.Context,
+	stdout, stderr io.Writer,
+) error {
+	mux, _ := NewServer()
+	srv := &http.Server{
+		Addr:    fmt.Sprintf("0.0.0.0:%s", port),
+		Handler: mux,
+	}
+
+	// Start the server
+	go func() {
+		fmt.Fprintf(stdout, "listening on %s\n", srv.Addr)
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(stderr, "error listening and serving: %s\n", err)
+		}
+	}()
+
+	// Graceful shutdown
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		<-ctx.Done()
+
+		// 10 second time limit for shutting down
+		shutdownCtx := context.Background()
+		shutdownCtx, cancel := context.WithTimeout(shutdownCtx, 10*time.Second)
+		defer cancel()
+		if err := srv.Shutdown(shutdownCtx); err != nil && err != http.ErrServerClosed {
+			fmt.Fprintf(stderr, "error shutting down http server: %s\n", err)
+		}
+	}()
+	wg.Wait()
+
+	return nil
+}
+
+func NewServer() (*http.ServeMux, huma.API) {
+	mux := http.NewServeMux()
+	api := humago.New(mux, huma.DefaultConfig("Compile Service", "0.1.0"))
+	// add routes
+	return mux, api
 }
