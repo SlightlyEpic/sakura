7a3b814014af1d1bd0d1ea5e54877b9ff6332433
SlightlyEpic <42976178+SlightlyEpic@users.noreply.github.com>
2025-07-17 18:19:58 +0530

    feat: more project routes

--- web/server/api/project.get.ts
+++ web/server/api/project.get.ts
@@ -4,10 +4,6 @@ import { auth } from "~/server/lib/auth";
 import { db } from "~/server/database/db";
 import { sql } from "drizzle-orm";
 
-const routeParams = z.object({
-    projectId: z.string().min(1),
-});
-
 export default defineEventHandler(async (event) => {
     const session = await auth.api.getSession(event);
 
@@ -18,14 +14,11 @@ export default defineEventHandler(async (event) => {
         };
     }
 
-    const params = await getValidatedRouterParams(event, routeParams.parse);
-
-    const project = await db.query.project.findFirst({
+    const projects = await db.query.project.findMany({
         where: (fields, operators) => {
-            return operators.eq(fields.id, sql.placeholder(params.projectId)) &&
-                operators.eq(fields.ownerId, sql.placeholder(session.user.id))
+            return operators.eq(fields.ownerId, sql.placeholder(session.user.id))
         },
     })
     
-    return project;
+    return projects;
 });
